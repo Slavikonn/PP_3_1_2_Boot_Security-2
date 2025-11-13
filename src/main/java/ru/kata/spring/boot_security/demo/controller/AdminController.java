5d82cd1d321a3198fbcf6b35876f54f72296793b
Slavikonn <slavikon132@gmail.com>
2025-11-13 11:48:07 +0200

    update AdminController and views to Bootstrap

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -9,41 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.AdminService;
+import ru.kata.spring.boot_security.demo.service.UserService;
 
-import java.util.List;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final UserService userService;
     private final AdminService adminService;
 
-    public AdminController(AdminService adminService) {
+    public AdminController(UserService userService, AdminService adminService) {
+        this.userService = userService;
         this.adminService = adminService;
     }
 
     @GetMapping
-    public String showAdminPage(Model model) {
-        List<User> users = adminService.findAll();
-        model.addAttribute("users", users);
-        return "admin-page";
-    }
-
-    @GetMapping("/addUser")
-    public String showAddUserPage(Model model) {
-        model.addAttribute("user", new User());
-        return "add-user-page";
+    public String showAdminPage(Principal principal, Model model) {
+        model.addAttribute("user", userService.findUserByUsername(principal.getName()));
+        model.addAttribute("users", adminService.findAll());
+        return "pages/admin-page";
     }
 
     @PostMapping("/addUser")
     public String addUser(@ModelAttribute User user) {
         adminService.createUser(user);
         return "redirect:/admin";
-    }
-
-    @GetMapping("/updateUser")
-    public String showUpdateUserPage(@RequestParam("id") Long id, Model model) {
-        model.addAttribute("user", adminService.findById(id));
-        return "update-user-page";
     }
 
     @PostMapping("/updateUser")
