b2fb13f64640eecb754cf039e68717bb490e410e
Slavikonn <slavikon132@gmail.com>
2025-11-17 14:07:27 +0200

    convert UserController to REST API

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
@@ -0,0 +1,29 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.dto.UserDto;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.util.UserMapper;
+
+@RestController
+@RequestMapping("api/user")
+public class UserRestController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    public UserRestController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<UserDto> getCurrentUser(Authentication authentication) {
+        User user = userService.findUserByUsername(authentication.getName());
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+}
