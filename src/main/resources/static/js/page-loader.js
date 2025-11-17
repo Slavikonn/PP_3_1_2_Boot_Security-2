527f7dbccd9938b299daa92de1ef6793d47df04e
Slavikonn <slavikon132@gmail.com>
2025-11-17 14:03:48 +0200

    refactor user-page and add page-loader.js

---
+++ src/main/resources/static/js/page-loader.js
@@ -0,0 +1,42 @@
+function loadFragment(id, url, callback) {
+    fetch(url)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById(id).innerHTML = html;
+            if (callback) callback();
+        });
+}
+
+function loadCurrentUser() {
+    fetch("/api/user")
+        .then(res => res.json())
+        .then(user => {
+            const roles = user.roles.map(r => r.replace("ROLE_", "")).join(" ");
+            const userInfoElement = document.getElementById("navbarUserInfo");
+            userInfoElement.innerHTML = `<span style="font-weight: bold;">${user.email}</span> with roles: ${roles}`;
+        })
+}
+
+function loadUserInfo() {
+    const tbody = document.querySelector("#singleUserTable tbody");
+    fetch("/api/user")
+        .then(res => res.json())
+        .then(user => {
+            tbody.innerHTML = "";
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${user.id}</td>
+                <td>${user.username}</td>
+                <td>${user.surname}</td>
+                <td>${user.age}</td>
+                <td>${user.email}</td>
+                <td>${user.roles.map(r => r.replace("ROLE_", "")).join(", ")}</td>
+            `;
+            tbody.appendChild(row);
+        })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadFragment("navbar-container", "/fragments/navbar.html", loadCurrentUser);
+    loadFragment("user-info", "/fragments/tables/user-info.html", loadUserInfo);
+});
